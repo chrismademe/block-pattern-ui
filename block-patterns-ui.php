c068eaf3b88bc10b469cd3509fe1b2c7dbc75f22
Chris Galbraith <chris@resknow.co.uk>
2021-08-27 14:38:10 +0100

    Bump version for 0.3.0

--- block-patterns-ui.php
+++ block-patterns-ui.php
@@ -6,7 +6,7 @@
  * Description: A simple UI for creating and managing Block Patterns
  * Author: Chris Galbraith
  * Author URI: https://chrisgalbraith.com
- * Version: 0.2.0
+ * Version: 0.3.0
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: bpui
