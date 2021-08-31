9465e050509bfbc294cbbb3d1306ca6b66309634
Chris Galbraith <chris@resknow.co.uk>
2021-08-31 13:22:32 +0100

    Bundle Select2 instead of using a CDN link

--- lib/admin-assets.php
+++ lib/admin-assets.php
@@ -12,10 +12,9 @@ add_action( 'admin_enqueue_scripts', function() {
     if ( in_array( $screen->id, $screens ) ) {
         wp_enqueue_style( 'bpui-admin', BlockPatternsUI::uri() . 'frontend/dist/admin.min.css' );
 
-        wp_enqueue_script( 'select2', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js', ['jquery'] );
+        wp_enqueue_script( 'select2', BlockPatternsUI::uri() . 'frontend/dist/vendor.select2.min.js', ['jquery'] );
         wp_enqueue_script( 'bpui-edit-screen', BlockPatternsUI::uri() . 'frontend/dist/edit-screen.min.js', ['jquery', 'select2'] );
 
-        wp_enqueue_style( 'select2', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css' );
         wp_enqueue_style( 'bpui-admin', BlockPatternsUI::uri() . 'frontend/dist/edit-screen.min.css' );
     }
 } );
